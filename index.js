0a540f520d21c703abd51cfc10f4e1d59a057825
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-23 20:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(n) {
   if(n <= 400) {
-    return "This one is on me!";
+    return "This one is on me!"
   } else if (2500 > n > 2000) {
     return "I will gladly take your thirty bucks."
   } else {
-    return "No can do.";
+    return "No can do."
   }
 }
