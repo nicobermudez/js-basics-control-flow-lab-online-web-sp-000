ad1bd0bbc674717d44bdf03332fc8347911bebfe
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-23 20:48:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(n) {
+  if(n <= 400) {
+    return "This one is on me!"
+  } else if (2000 <= n && n < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city == "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+}
