f419a8ecb635396fbdf17751cdc5845694ae09f9
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-23 20:44:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(n) {
   if(n <= 400) {
     return "This one is on me!";
-  } else if (n > 2000) {
+  } else if (2500 > n > 2000) {
     return "I will gladly take your thirty bucks."
-  }
+  } else
 }
